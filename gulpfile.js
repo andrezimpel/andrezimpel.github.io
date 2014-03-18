21fb8534ae006deebb6dd93d35cd93ae783b505e
Andre Zimpel <az@yum.de>
2014-03-18 18:44:08 +0100

    a lot of content

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    path = require('path');
+
+
+// ------------------------------------------
+
+
+// paths
+var paths = {
+  scripts: ['js/**/*.js'],
+  images: 'img/**/*',
+  sass: "scss/**/*.scss",
+  html: "index.html",
+  public_dist: "dist/**/*.*"
+};
+
+
+// ------------------------------------------
+
+
+// Retrun the task when a file changes
+gulp.task('watch', function () {
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.public_dist, ['public_dist']);
+});
+
+
+
+// copy index to /public for developing with pow
+gulp.task('html', function () {
+  gulp.src(paths.html)
+    .pipe(gulp.dest('./public/'));
+});
+
+
+
+// compile sass to css and store it in dist
+gulp.task('sass', function () {
+  gulp.src('./scss/andrezimpel.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+
+
+gulp.task('scripts', function() {
+  gulp.src([
+            './bower_components/jquery/dist/jquery.js',
+            './bower_components/bootstrap-sass/js/transition.js',
+            './bower_components/bootstrap-sass/js/alert.js',
+            './bower_components/bootstrap-sass/js/button.js',
+            './bower_components/bootstrap-sass/js/carousel.js',
+            './bower_components/bootstrap-sass/js/collapse.js',
+            './bower_components/bootstrap-sass/js/dropdown.js',
+            './bower_components/bootstrap-sass/js/modal.js',
+            './bower_components/bootstrap-sass/js/tooltip.js',
+            './bower_components/bootstrap-sass/js/popover.js',
+            './bower_components/bootstrap-sass/js/scrollspy.js',
+            './bower_components/bootstrap-sass/js/tab.js',
+            './bower_components/bootstrap-sass/js/affix.js',
+            "./javascripts/main.js"
+            ])
+    // .pipe(uglify())
+    .pipe(concat('andrezimpel.min.js'))
+    .pipe(gulp.dest('./dist/js'));
+});
+
+
+
+// copy files from dist to public dist as they change
+gulp.task('public_dist', function () {
+  gulp.src(paths.public_dist)
+    .pipe(gulp.dest('./public/dist'));
+});
+
+// ------------------------------------------
+
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['sass','html','public_dist', 'scripts', 'watch']);
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+// $(document).ready(function(){
+//   user = "andrezimpel";
+//   jQuery.getJSON("http://twitter.com/statuses/user_timeline/"+ user +".json?callback=?&count=3", function(data) {
+//     jQuery.each(data, function(i, object) {
+//         console.log(data);
+//     });
+//   });
+// });
